161b053e849ba0d471722c348d551a45e765071c
kaimu <ok89.bado@gmail.com>
2020-03-24 19:57:27 +0900

    ホーム画面に4分割しカードを簡単に追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,78 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
-    <div class="header bg-gradient-primary py-7 py-lg-8">
-        <div class="container">
-            <div class="header-body text-center mt-7 mb-7">
-                <div class="row justify-content-center">
-                    <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
-                    </div>
-                </div>
-            </div>
+<div class="header bg-gradient-primary py-7 py-lg-8">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top"
+            src="https://user-images.githubusercontent.com/49427056/77417707-19830780-6e09-11ea-9e1b-97586388290f.jpg"
+            alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
-        <div class="separator separator-bottom separator-skew zindex-100">
-            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-            </svg>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top"
+            src="https://user-images.githubusercontent.com/49427056/77417707-19830780-6e09-11ea-9e1b-97586388290f.jpg"
+            alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top"
+            src="https://user-images.githubusercontent.com/49427056/77417707-19830780-6e09-11ea-9e1b-97586388290f.jpg"
+            alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img class="card-img-top"
+            src="https://user-images.githubusercontent.com/49427056/77417707-19830780-6e09-11ea-9e1b-97586388290f.jpg"
+            alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="separator separator-bottom separator-skew zindex-100">
+    <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+    </svg>
+  </div>
+</div>
 
-    <div class="container mt--10 pb-5"></div>
+<div class="container mt--10 pb-5"></div>
+<div class="container">
+  <div class="row">
+    <div class="col"><span>col</span></div>
+    <div class="col"><span>col</span></div>
+    <div class="col"><span>col</span></div>
+    <div class="col"><span>col</span></div>
+  </div>
+</div>
 @endsection
