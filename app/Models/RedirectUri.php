4a599f63c132e913faec6ddb0ef283351306555f
Okita <keisi1100525@outlook.jp>
2020-03-30 23:51:53 +0900

    👍 Update Migrations and Models
    DB設計修正 #63
    
    - [x] Modelにリレーション定義
    - [x] ClientListテーブルの削除、それに伴う変更
    - [x] SearchConditionsテーブルの追加. 検索結果を保存できるようにする
    - [x] CircleCIの不要なコマンド削除

--- app/Models/RedirectUri.php
+++ app/Models/RedirectUri.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RedirectUri extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
