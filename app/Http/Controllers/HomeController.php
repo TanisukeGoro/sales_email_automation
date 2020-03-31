33dfc27ab6f30ca677ac41003fcff4ad9edb48d2
kaimu <ok89.bado@gmail.com>
2020-03-31 15:03:38 +0900

    コード整形

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-  public function index()
-  {
-    return view('dashboard');
-  }
+    public function index()
+    {
+        return view('dashboard');
+    }
 }
