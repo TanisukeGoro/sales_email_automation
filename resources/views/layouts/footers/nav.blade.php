a8ca22db3922ea5d2d2cbdc5ab1e8ffccf231016
shunsuke <jirudore@gmail.com>
2020-04-02 02:31:09 +0900

    nav.blade.phpのフッター文字をenvのAPP_NAMEから取得するように変更

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,8 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://twitter.com/okita_kamegoro" class="font-weight-bold ml-1" target="_blank">Abe Keishi</a> &amp;
-            <a href="#" class="font-weight-bold ml-1" target="_blank">Nakanowatari Kei</a>
+            &copy; {{ now()->year }} <a href="#" class="font-weight-bold ml-1" target="_blank">{{ config('app.name') }}</a>
         </div>
     </div>
     <div class="col-xl-6">
