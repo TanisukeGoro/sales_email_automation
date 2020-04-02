fe57588545786c6c23c42770fa95ac7112927fc2
kaimu <ok89.bado@gmail.com>
2020-04-02 20:04:59 +0900

    companyテーブルのseeder追加

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -1,13 +1,20 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Models\Company;
+use App\Models\CompanyCategory;
+// @var \Illuminate\Database\Eloquent\Factory $factory
+
+use App\Models\ListingStock;
 use Faker\Generator as Faker;
 
 $factory->define(Company::class, function (Faker $faker) {
     return [
         'name' => $faker->company,
         'code' => '',
+        'listing_stock_id' => \mt_rand(1, ListingStock::all()->count()),
+        'company_category_id' => \mt_rand(1, CompanyCategory::all()->count()),
+        'address' => $faker->address,
+        'n_employees' => \mt_rand(100, 30000),
         'top_url' => 'http://',
         'form_url' => 'http://',
     ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
             CompanyCategoriesTableSeeder::class,
             ListingStocksTableSeeder::class,
             UsersTableSeeder::class,
-            // CompaniesTableSeeder::class,
+            CompaniesTableSeeder::class,
             TemplatesTableSeeder::class,
             SentsTableSeeder::class,
             RedirectUrisTableSeeder::class,
