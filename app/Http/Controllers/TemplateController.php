7f153349c4221815442881b64e47c2619cfad8d6
kaimu <ok89.bado@gmail.com>
2020-04-06 00:11:34 +0900

    テンプレート作成後リダイレクト設定

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -16,7 +16,7 @@ class TemplateController extends Controller
      */
     public function index()
     {
-        $templates = Auth::user()->templates()->get();
+        $templates = Auth::user()->templates()->orderBy('created_at', 'asc')->get();
 
         return view('template.index', [
             'templates' => $templates,
@@ -52,6 +52,8 @@ class TemplateController extends Controller
         $template = new Template();
         $template->user_id = Auth::id();
         $template->fill($request->all())->save();
+
+        return redirect()->route('template.index');
     }
 
     /**
