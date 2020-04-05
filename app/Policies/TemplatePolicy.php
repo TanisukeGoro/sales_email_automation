70b4465fa72aae7e239ca685852dc4a86570262e
kaimu <ok89.bado@gmail.com>
2020-04-06 00:57:55 +0900

    policy追加

---
+++ app/Policies/TemplatePolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Template;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TemplatePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any templates.
+     */
+    public function viewAny(User $user)
+    {
+    }
+
+    /**
+     * Determine whether the user can view the template.
+     *
+     * @param \App\Template $template
+     */
+    public function view(User $user, Template $template)
+    {
+    }
+
+    /**
+     * Determine whether the user can create templates.
+     */
+    public function create(User $user)
+    {
+    }
+
+    /**
+     * Determine whether the user can update the template.
+     *
+     * @param \App\Template $template
+     */
+    public function update(User $user, Template $template)
+    {
+        return $user->id === $template->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the template.
+     *
+     * @param \App\Template $template
+     */
+    public function delete(User $user, Template $template)
+    {
+        return $user->id === $template->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the template.
+     *
+     * @param \App\Template $template
+     */
+    public function restore(User $user, Template $template)
+    {
+    }
+
+    /**
+     * Determine whether the user can permanently delete the template.
+     *
+     * @param \App\Template $template
+     */
+    public function forceDelete(User $user, Template $template)
+    {
+    }
+}
