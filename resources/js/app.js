33b30754a83b4f752abacd2bf64ac0fb1a9d3bf0
kaimu <ok89.bado@gmail.com>
2020-04-14 22:28:32 +0900

    pusherで多重ログインを通知

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import './bootstrap'
 import Vue from 'vue'
 import SuggestInput from './components/SuggestInput.vue'
 
+window.Echo.channel("channel-message").listen("MessageEvent", function (data) {
+  alert(data["message"]);
+  window.location = '/login';
+});
+
 const newVue = (option) => new Vue(option)
 
 document.addEventListener("DOMContentLoaded", () => {
