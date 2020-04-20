d94a632078ec5b4e33e8d7e27115658d570a6565
kaimu <ok89.bado@gmail.com>
2020-04-21 00:30:50 +0900

    uriクリックカウントのテーブル作成・カウントロジック・ルート・redirectUriのロジック作成・表示

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -3,91 +3,108 @@
 namespace App\Http\Controllers;
 
 use App\Models\RedirectUri;
+use App\Models\UrlClickCount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class RedirectController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    //
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
 
-  public function publish()
-  {
-    $redirect_uri = new RedirectUri();
-    $redirect_uri->user_id = auth()->id();
-    $redirect_uri->uri = config("app", "url");
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    //
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function publish()
+    {
+        $redirect_uri = new RedirectUri();
+        $redirect_uri->user_id = auth()->id();
+        $redirect_uri->uri = route('redirect-link.index') . '/' . Str::uuid();
+        $redirect_uri->save();
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    //
-  }
+        return redirect()->route('profile.edit')->with('create_url', 'URLが発行されました！');
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\RedirectUri  $redirectUri
-   * @return \Illuminate\Http\Response
-   */
-  public function show(RedirectUri $redirectUri)
-  {
-    //
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\RedirectUri $redirectUri
+     * @param mixed $uuid
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function count($uuid)
+    {
+        if (!RedirectUri::where('uri', route('redirect-link.index') . '/' . $uuid)->exists()) {
+            return redirect()->route('home');
+        }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  \App\RedirectUri  $redirectUri
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(RedirectUri $redirectUri)
-  {
-    //
-  }
+        $user_id = RedirectUri::where('uri', route('redirect-link.index') . '/' . $uuid)->first()->user_id;
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\RedirectUri  $redirectUri
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, RedirectUri $redirectUri)
-  {
-    //
-  }
+        if (UrlClickCount::where('user_id', $user_id)->exists()) {
+            $uri_click_count = UrlClickCount::where('user_id', $user_id)->first();
+            ++$uri_click_count->count;
+            $uri_click_count->save();
+        } else {
+            $uri_click_count = new UrlClickCount();
+            $uri_click_count->user_id = $user_id;
+            $uri_click_count->count = 1;
+            $uri_click_count->save();
+        }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\RedirectUri  $redirectUri
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(RedirectUri $redirectUri)
-  {
-    //
-  }
+        return redirect()->route('home');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\RedirectUri $redirectUri
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RedirectUri $redirectUri)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\RedirectUri $redirectUri
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RedirectUri $redirectUri)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\RedirectUri $redirectUri
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RedirectUri $redirectUri)
+    {
+    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,22 @@
 import './bootstrap'
 import Vue from 'vue'
 import SuggestInput from './components/SuggestInput.vue'
+import RedirectUri from "./components/RedirectUri.vue"
 
 const newVue = option => new Vue(option)
 
 document.addEventListener('DOMContentLoaded', () => {
   newVue({
     el: '#vue-app',
-    components: {}
+    components: {
+      RedirectUri
+    }
   })
 
   newVue({
     el: '#vue-sidebar', // コンポーネントをDOMのIDで使い分ける場合
     components: {
-      SuggestInput
+      SuggestInput,
     }
   })
 })
