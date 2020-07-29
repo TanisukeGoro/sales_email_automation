aa68319693cfb948dde8f891b1cde1fcc9b224db
kaimu <ok89.bado@gmail.com>
2020-07-29 21:06:38 +0900

    差分修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,5 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
