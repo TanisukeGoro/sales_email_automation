a6bd4f3f052bb26a5e3fecb0a1973f5f57babcc1
Okita <keisi1100525@outlook.jp>
2020-07-30 16:00:12 +0900

    fix

--- database/factories/ApproachFolderFactory.php
+++ database/factories/ApproachFolderFactory.php
@@ -6,10 +6,9 @@ use App\Models\ApproachFolder;
 use Faker\Generator as Faker;
 
 $factory->define(ApproachFolder::class, function (Faker $faker) {
-    $title = ['あいうえお', '農業上場', '情報企業', '大工', 'セントラル', '大企業', 'ゲーム', '一次産業', '二次産業', 'サービス業', 'PC'];
-
     return [
         'user_id' => 1,
-        'title' => $title[\mt_rand(0, \count($title) - 1)],
+        'title' => join(' ', $faker->unique->words(3))
+        ,
     ];
 });
