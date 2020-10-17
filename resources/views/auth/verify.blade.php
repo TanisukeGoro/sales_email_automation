14e2a3cc4beba68296d24be1e3290447d6c4b1c7
kaimu <ok89.bado@gmail.com>
2020-10-18 01:46:50 +0900

    メール文を日本語に修正

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -13,7 +13,7 @@
           <div>
             @if (session('resent'))
             <div class="alert alert-success" role="alert">
-              {{ __('A fresh verification link has been sent to your email address.') }}
+              新しいリンクをあなたのメールアドレスに送信しました。
             </div>
             @endif
 
